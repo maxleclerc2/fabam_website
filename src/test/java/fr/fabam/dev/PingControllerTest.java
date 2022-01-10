d898f8e98c464c9516697294e60ba92c1b2e4105
maxleclerc2 <mleclerc@et.esiea.fr>
2022-01-10 18:55:24 +0100

    Added ping controller

---
+++ src/test/java/fr/fabam/dev/PingControllerTest.java
@@ -0,0 +1,34 @@
+package fr.fabam.dev;
+
+import fr.fabam.Launcher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@SpringBootTest(classes = Launcher.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+class PingControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new PingController()).build();
+    }
+
+    @Test
+    void ping_respond_OK() throws Exception {
+
+        mockMvc
+            .perform(MockMvcRequestBuilders.get("/api/ping"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("OK"));
+    }
+}
